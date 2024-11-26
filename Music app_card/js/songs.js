62d62f344e9dd11f54fcda2582c4c492f56a3389
Jie Yu <jyu238@myseneca.ca>
2024-11-26 13:51:22 -0500

    web programming

---
+++ Music app_card/js/songs.js
@@ -0,0 +1,513 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  /* TODO */
+  {
+    songId: "S-ID-00001",
+    artistId: "A-ID-00001",
+    title: "Believer",
+    year: "2017",
+    length: 204,
+    link: "https://www.youtube.com/watch?v=7wtfhZwyrcc",
+    imageUrl: "https://wallpapercave.com/wp/wp2741342.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00002",
+    artistId: "A-ID-00001",
+    title: "Whatever It Takes",
+    year: "2017",
+    length: 210,
+    link: "https://www.youtube.com/watch?v=gOsM-DYAEhY",
+    imageUrl: "https://i.pinimg.com/736x/3b/92/e7/3b92e7fdd75a3f27eff76469b9cafb74.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00003",
+    artistId: "A-ID-00001",
+    title: "Radioactive",
+    year: "2012",
+    length: 207,
+    link: "https://www.youtube.com/watch?v=ktvTqknDobU",
+    imageUrl: "https://images.genius.com/a1632bc91da7a8b8a1cc63dc3b65953f.1000x1000x1.png",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00004",
+    artistId: "A-ID-00001",
+    title: "Thunder",
+    year: "2017",
+    length: 180,
+    link: "https://www.youtube.com/watch?v=fKopy74weus",
+    imageUrl: "https://i.redd.it/zv8rcdwob0k81.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+
+  {
+    songId: "S-ID-00041",
+    artistId: "A-ID-00001",
+    title: "Bones",
+    year: "2022",
+    length: 165,
+    link: "https://www.youtube.com/watch?v=DYed5whEf4g",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/b/bb/Imagine_Dragons_Bones_cover.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+
+  {
+    songId: "S-ID-00042",
+    artistId: "A-ID-00001",
+    title: "It's Time",
+    year: "2011",
+    length: 246,
+    link: "https://www.youtube.com/watch?v=sENM2wA_FTg",
+    imageUrl:
+      "https://i.discogs.com/s7UZz3a0sSlh3bexP6CwYz4kxZYxJ5HOZk2I_ELL3u8/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE1MzU1/NDk5LTE1OTAyMjc0/MzMtNDAxNi5qcGVn.jpeg",
+    lang: ["en"],
+    explicit: true
+  },
+
+  {
+    songId: "S-ID-00005",
+    artistId: "A-ID-00002",
+    title: "Counting Stars",
+    year: "2013",
+    length: 283,
+    link: "https://www.youtube.com/watch?v=hT_nvWreIhg",
+    imageUrl:
+      "https://cdn-images.dzcdn.net/images/cover/016f3117546e2bcc4229142c7268fb04/0x1900-000000-80-0-0.jpg",
+    lang: ["en"],
+    explicit: false
+  },
+  {
+    songId: "S-ID-00006",
+    artistId: "A-ID-00002",
+    title: "Love Runs Out",
+    year: "2014",
+    length: 228,
+    link: "https://www.youtube.com/watch?v=0OWj0CiM8WU",
+    imageUrl:
+      "https://cdns-images.dzcdn.net/images/cover/21e1ee13090e921a728ce38eca7e1d92/1900x1900-000000-80-0-0.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00007",
+    artistId: "A-ID-00002",
+    title: "I Lived",
+    year: "2014",
+    length: 339,
+    link: "https://www.youtube.com/watch?v=z0rxydSolwU",
+    imageUrl: "https://i.scdn.co/image/ab67616d0000b273e80e7dbce3996a1ae5967751",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00008",
+    artistId: "A-ID-00002",
+    title: "Good Life",
+    year: "2010",
+    length: 248,
+    link: "https://www.youtube.com/watch?v=jZhQOvvV45w",
+    imageUrl: "https://i.scdn.co/image/ab67616d0000b27330df2c46363591fd26f0e465",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00009",
+    artistId: "A-ID-00003",
+    title: "Centuries",
+    year: "2014",
+    length: 210,
+    link: "https://www.youtube.com/watch?v=LBr7kECsjcQ",
+    imageUrl: "https://i1.sndcdn.com/artworks-000660917911-z2i7qh-t500x500.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00010",
+    artistId: "A-ID-00003",
+    title: "Light Em Up",
+    year: "2013",
+    length: 187,
+    link: "https://www.youtube.com/watch?v=LkIWmsP3c_s",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/en/2/2d/Fall_Out_Boy_-_%22My_Songs_Know_What_You_Did_in_the_Dark_%28Light_Em_Up%29%22.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00011",
+    artistId: "A-ID-00003",
+    title: "The Phoenix",
+    year: "2013",
+    length: 262,
+    link: "https://www.youtube.com/watch?v=5hDZbroaQDc",
+    imageUrl: "https://i.scdn.co/image/ab67616d0000b27360c84f44c080824ce336e1a1",
+    lang: ["en"],
+    explicit: false
+  },
+  {
+    songId: "S-ID-00012",
+    artistId: "A-ID-00003",
+    title: "Irresistible",
+    year: "2015",
+    length: 253,
+    link: "https://www.youtube.com/watch?v=i8BGAYXNW74",
+    imageUrl: "https://i1.sndcdn.com/artworks-000148311781-9zhzbp-t1080x1080.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00013",
+    artistId: "A-ID-00004",
+    title: "High Hopes",
+    year: "2018",
+    length: 214,
+    link: "https://www.youtube.com/watch?v=IPXIgEAGe4U",
+    imageUrl:
+      "https://cdn-images.dzcdn.net/images/cover/5796eef07117a9ac694fa8074767526e/1900x1900-000000-80-0-0.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00014",
+    artistId: "A-ID-00004",
+    title: "Victorious",
+    year: "2016",
+    length: 219,
+    link: "https://www.youtube.com/watch?v=AUChk0lxF44",
+    imageUrl: "https://i1.sndcdn.com/artworks-jrWDsrwwD0Knm8zS-SRiFmA-t1080x1080.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00015",
+    artistId: "A-ID-00004",
+    title: "This Is Gospel",
+    year: "2013",
+    length: 190,
+    link: "https://www.youtube.com/watch?v=tGE381tbQa8",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXaUs7xAQTfyrfzqBqle7-9XBfDt6Jdivg6w&s",
+    lang: ["en"],
+    explicit: false
+  },
+  {
+    songId: "S-ID-00016",
+    artistId: "A-ID-00004",
+    title: "Emperor's New Clothes",
+    year: "2015",
+    length: 219,
+    link: "https://www.youtube.com/watch?v=7qFF2v8VsaA",
+    imageUrl: "https://i1.sndcdn.com/artworks-IFShjsg6sfvd-0-t500x500.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00017",
+    artistId: "A-ID-00005",
+    title: "Mr. Brightside",
+    year: "2003",
+    length: 196,
+    link: "https://www.youtube.com/watch?v=gGdGFtwCNBE",
+    imageUrl: "https://i1.sndcdn.com/artworks-000657796777-ux30fc-t1080x1080.jpg",
+    lang: ["en"],
+    explicit: false
+  },
+  {
+    songId: "S-ID-00018",
+    artistId: "A-ID-00005",
+    title: "When You Were Young",
+    year: "2006",
+    length: 316,
+    link: "https://www.youtube.com/watch?v=ff0oWESdmH0",
+    imageUrl: "https://i.scdn.co/image/ab67616d0000b2734f87f14089217e3f70a5f39e",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00019",
+    artistId: "A-ID-00005",
+    title: "All These Things That I've Done",
+    year: "2004",
+    length: 280,
+    link: "https://www.youtube.com/watch?v=sZTpLvsYYHw",
+    imageUrl:
+      "https://m.media-amazon.com/images/M/MV5BYzFjNDBlYzctYThhZS00NmYwLWJjMjItMjU0ZTRmMDZlZmRlXkEyXkFqcGc@._V1_.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00020",
+    artistId: "A-ID-00005",
+    title: "Human",
+    year: "2008",
+    length: 254,
+    link: "https://www.youtube.com/watch?v=8yGHyIBYAgg",
+    imageUrl: "https://images.genius.com/c2e5c279c0668b9f22d045b9cca1bc08.1000x1000x1.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00021",
+    artistId: "A-ID-00006",
+    title: "Kings and Queens",
+    year: "2009",
+    length: 530,
+    link: "https://www.youtube.com/watch?v=hTMrlHHVx8A",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/en/4/48/30_seconds_to_mars_kings_and_queens.png",
+    lang: ["en"],
+    explicit: false
+  },
+  {
+    songId: "S-ID-00022",
+    artistId: "A-ID-00006",
+    title: "This Is War",
+    year: "2009",
+    length: 360,
+    link: "https://www.youtube.com/watch?v=Zcps2fJKuAI",
+    imageUrl:
+      "https://i.discogs.com/m7_GYhhZOiVog07lb9tkSdjavyciX4ktPspwbekP7y4/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTQyMDI4/ODMtMTM1ODQ0MTY5/NS05NzM5LmpwZWc.jpeg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00023",
+    artistId: "A-ID-00006",
+    title: "Do or Die",
+    year: "2010",
+    length: 422,
+    link: "https://www.youtube.com/watch?v=GC63HGcsfEg",
+    imageUrl: "https://i1.sndcdn.com/artworks-000103809775-a7xqy1-t500x500.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00024",
+    artistId: "A-ID-00006",
+    title: "Closer to the Edge",
+    year: "2010",
+    length: 382,
+    link: "https://www.youtube.com/watch?v=mLqHDhF-O28",
+    imageUrl: "https://i1.sndcdn.com/artworks-000213331615-7svih9-t500x500.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00025",
+    artistId: "A-ID-00007",
+    title: "Ain't It Fun",
+    year: "2014",
+    length: 217,
+    link: "https://www.youtube.com/watch?v=EFEmTsfFL5A",
+    imageUrl:
+      "https://cdns-images.dzcdn.net/images/cover/bb3fb302541c9ea8a22e59d064528b38/1900x1900-000000-80-0-0.jpg",
+    lang: ["en"],
+    explicit: false
+  },
+  {
+    songId: "S-ID-00026",
+    artistId: "A-ID-00007",
+    title: "Misery Business",
+    year: "2007",
+    length: 199,
+    link: "https://www.youtube.com/watch?v=aCyGvGEtOwc",
+    imageUrl: "https://lastfm.freetls.fastly.net/i/u/ar0/b7a4b3000d0c431fbce299986ac51c48.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00027",
+    artistId: "A-ID-00007",
+    title: "Hard Times",
+    year: "2017",
+    length: 198,
+    link: "https://www.youtube.com/watch?v=AEB6ibtdPZc",
+    imageUrl: "https://i1.sndcdn.com/artworks-z9LfbdXkyPsd5Ayy-skHsjQ-t500x500.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00028",
+    artistId: "A-ID-00007",
+    title: "Still Into You",
+    year: "2013",
+    length: 220,
+    link: "https://www.youtube.com/watch?v=OblL026SvD4",
+    imageUrl: "https://i1.sndcdn.com/artworks-000043331076-hsb36i-t500x500.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00029",
+    artistId: "A-ID-00008",
+    title: "Best of You",
+    year: "2005",
+    length: 255,
+    link: "https://www.youtube.com/watch?v=h_L4Rixya64",
+    imageUrl:
+      "https://m.media-amazon.com/images/M/MV5BZDYyMzhhZTYtNTEyMC00NmI2LWIzNDUtODY0ZTVjYWQ0MGJiXkEyXkFqcGc@._V1_.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00030",
+    artistId: "A-ID-00008",
+    title: "Learn to Fly",
+    year: "1999",
+    length: 448,
+    link: "https://www.youtube.com/watch?v=JozAmXo2bDE",
+    imageUrl:
+      "https://m.media-amazon.com/images/M/MV5BNzdiNzRiZmEtOTk2Yy00N2VmLTk5ZDktOTNhOTRmYzA4MWEyXkEyXkFqcGc@._V1_.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00031",
+    artistId: "A-ID-00008",
+    title: "Walk",
+    year: "2011",
+    length: 358,
+    link: "https://www.youtube.com/watch?v=4PkcfQtibmU",
+    imageUrl: "https://i.pinimg.com/736x/f1/85/c9/f185c9789e35e7c9e95f4537bd4fa7ce.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00032",
+    artistId: "A-ID-00008",
+    title: "Times Like These",
+    year: "2002",
+    length: 267,
+    link: "https://www.youtube.com/watch?v=rhzmNRtIp8k",
+    imageUrl: "https://images.eil.com/large_image/FOO_FIGHTERS_TIMES%2BLIKE%2BTHESE-632196.jpg",
+    lang: ["en"],
+    explicit: false
+  },
+  {
+    songId: "S-ID-00033",
+    artistId: "A-ID-00009",
+    title: "Viva La Vida",
+    year: "2008",
+    length: 242,
+    link: "https://www.youtube.com/watch?v=dvgZkm1xWPE",
+    imageUrl: "https://i.scdn.co/image/ab67616d0000b273e21cc1db05580b6f2d2a3b6e",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00034",
+    artistId: "A-ID-00009",
+    title: "Adventure of a Lifetime",
+    year: "2015",
+    length: 300,
+    link: "https://www.youtube.com/watch?v=QtXby3twMmI",
+    imageUrl: "https://i.scdn.co/image/ab67616d0000b2738ff7c3580d429c8212b9a3b6",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00035",
+    artistId: "A-ID-00009",
+    title: "A Sky Full of Stars",
+    year: "2014",
+    length: 253,
+    link: "https://www.youtube.com/watch?v=VPRjCeoBqrI",
+    imageUrl: "https://i.scdn.co/image/ab67616d0000b273e5a95573f1b91234630fd2cf",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00036",
+    artistId: "A-ID-00009",
+    title: "Hurts Like Heaven",
+    year: "2012",
+    length: 210,
+    link: "https://www.youtube.com/watch?v=C8KV0mzqTXY",
+    imageUrl:
+      "https://coldplaymag.wordpress.com/wp-content/uploads/2014/12/hurtslikeheaven.jpg?w=640",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00043",
+    artistId: "A-ID-00009",
+    title: "Up&Up",
+    year: "2015",
+    length: 250,
+    link: "https://www.youtube.com/watch?v=BPNTC7uZYrI",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeyn4F0CyKvTOBXfJMj0mDnnW1F_lwFEXojw&s",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00044",
+    artistId: "A-ID-00009",
+    title: "Higher Power",
+    year: "2021",
+    length: 256,
+    link: "https://www.youtube.com/watch?v=3lfnR7OhZY8",
+    imageUrl:
+      "https://media.pitchfork.com/photos/608a33343bbb6032f540a222/4:3/w_1940,h_1455,c_limit/coldplay.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00037",
+    artistId: "A-ID-00010",
+    title: "Everybody Talks",
+    year: "2012",
+    length: 234,
+    link: "https://www.youtube.com/watch?v=X5G9tIe84lE",
+    imageUrl: "https://f4.bcbits.com/img/a4272189855_10.jpg",
+    lang: ["en"],
+    explicit: false
+  },
+  {
+    songId: "S-ID-00038",
+    artistId: "A-ID-00010",
+    title: "Animal",
+    year: "2010",
+    length: 208,
+    link: "https://www.youtube.com/watch?v=gM7Hlg75Mlo",
+    imageUrl:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music128/v4/8a/70/6f/8a706f0e-1ccf-9f74-d6ed-bdd7a9eb995e/00602527395494.rgb.jpg/600x600bf-60.jpg",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00039",
+    artistId: "A-ID-00010",
+    title: "Sleeping With a Friend",
+    year: "2014",
+    length: 227,
+    link: "https://www.youtube.com/watch?v=BB7R0ZY9w94",
+    imageUrl:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/020cebda-a1c6-4e41-9d3b-e498a10404d5/d77iitt-2a42d461-0b37-4b66-8c20-cdb403b7c8e9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAyMGNlYmRhLWExYzYtNGU0MS05ZDNiLWU0OThhMTA0MDRkNVwvZDc3aWl0dC0yYTQyZDQ2MS0wYjM3LTRiNjYtOGMyMC1jZGI0MDNiN2M4ZTkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pmP7T9Y9noa3mZMIO0HcJ5-nJzzfTesyifbd7lB0f88",
+    lang: ["en"],
+    explicit: true
+  },
+  {
+    songId: "S-ID-00040",
+    artistId: "A-ID-00010",
+    title: "Your Surrender",
+    year: "2011",
+    length: 218,
+    link: "https://www.youtube.com/watch?v=JcEuRT7rrP4",
+    imageUrl: "https://i1.sndcdn.com/artworks-000064448436-gf5gj1-t500x500.jpg",
+    lang: ["en"],
+    explicit: true
+  }
+];
